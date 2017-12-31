2d13b19cd35569958a0f7757c5c2058aaba79e74
bwielk <bwielk@gmail.com>
2017-12-31 21:05:13 +0100

    Pizza rework ongoing

--- src/pizzas/Pizza.java
+++ src/pizzas/Pizza.java
@@ -5,22 +5,31 @@ import java.util.ArrayList;
 public abstract class Pizza {
 	
 	String name;
-	String dough;
-	String sauce;
-	ArrayList<String> toppings = new ArrayList<String>();
+	Dough dough;
+	Sauce sauce;
+	Vegetable vegetables[];
+	Pepperoni pepperoni;
+	Cheese cheese;
 	
-	public String getDough() {
+	public Dough getDough() {
 		return dough;
 	}
-	public String getSauce() {
+	public Sauce getSauce() {
 		return sauce;
 	}
 	
 	public String getName() {
 		return name;
 	}
-	public ArrayList<String> getToppings() {
-		return toppings;
+	public Vegetable[] getVegetables(){
+		return vegetables;
 	}
-
+	public Pepperoni getPepperoni() {
+		return pepperoni;
+	}
+	public Cheese getCheese() {
+		return cheese;
+	}
+	
+	abstract void prepare();
 }
